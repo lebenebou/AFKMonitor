e5b01416cef9ed69943f747230e491c2c38031c7
Youssef Yammine <lebenbou@gmail.com>
2023-10-26 11:27:21 +0300

    removed unused countDown() function

--- Utils/TimeUtils.py
+++ Utils/TimeUtils.py
@@ -8,15 +8,5 @@ def getCurrentEpochTime() -> int:
 def epochToLocal(epochTime: int) -> str:
     return time.strftime("%I:%M %p", time.localtime(epochTime))
 
-# count down in hh:mm:ss format
-def countDown(seconds: int):
-    
-    while seconds > 0:
-    
-        timeLeft = time.strftime("%H:%M:%S", time.gmtime(seconds))
-        print(timeLeft, end="\r")
-        time.sleep(1)
-        seconds -= 1
-
 def getCurrentDate() -> str:
     return datetime.datetime.now().strftime("%b_%d_%Y")
