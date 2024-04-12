ea8d5f47bad809cbf52a215bae7421b6a1f440ff
lebenebou <lebenebou@gmail.com>
2024-04-12 10:40:08 +0300

    optimized Memory Utils

--- Utils/MemoryUtils.py
+++ Utils/MemoryUtils.py
@@ -1,59 +1,39 @@
 
 from Utils.CmdUtils import runCommand
-
-class Process:
-
-    def __init__(self, name: str, memoryUsageKB: float):
-
-        self.name = name
-        self.memoryUsageKB = memoryUsageKB
-
-    def getMemoryUsageMB(self) -> float:
-        return self.memoryUsageKB / 1000
-
-def extractProcessFromLine(stdoutLine: str) -> Process:
-
-    stdoutLine = stdoutLine.replace('"', '')
-
-    fields = stdoutLine.split(",")
-
-    name = fields[0]
-    memUsageKbString = fields[-1].strip(" K").strip()
-    
-    if len(fields) > 5:
-        memUsageKbString = fields[-2] + memUsageKbString
-
-    memUsageKb = float(memUsageKbString)
-    return Process(name, memUsageKb)
-
-def getRunningProcesses() -> list[Process]:
-
-    commandOutput = runCommand("tasklist /fo csv /nh").stdout
-
-    processes = [extractProcessFromLine(line) for line in commandOutput.splitlines()]
-
-    memUsage = dict()
-    for process in processes:
-        
-        memUsage.setdefault(process.name, 0)
-        memUsage[process.name] += process.memoryUsageKB
-
-    processes = [Process(name, memUsage[name]) for name in memUsage]
-    return processes
+import re
 
 class MemoryState:
 
+    tasklistRegex = re.compile(r"^(\S+)\s+\d+\s+\S+\s+\d\s+(\S+) K$", re.MULTILINE)
+
     def __init__(self):
         
-        processList = getRunningProcesses()
+        tasklist = runCommand("tasklist /nh").stdout
 
-        self.runningProcesses = len(processList)
-        self.totalMemoryUsageMB = sum([process.getMemoryUsageMB() for process in processList])
+        memUsage = dict()
 
-        hungriestProcess = max(processList, key=lambda process: process.memoryUsageKB)
+        hungriestProcessName: str = None
+        hungriestProcessMemUsageKB: float = 0
+        totalMemUsageKB: float = 0
 
-        self.hungriestProcessName = hungriestProcess.name
-        self.hungriestProcessMemoryUsageMB = hungriestProcess.getMemoryUsageMB()
+        for reMatch in MemoryState.tasklistRegex.finditer(tasklist):
+
+            name, memKB = reMatch.group(1), float(reMatch.group(2).replace(",", ""))
+
+            memUsage.setdefault(name, 0)
+            memUsage[name] += memKB
+
+            totalMemUsageKB += memKB
+
+            if memUsage[name] > hungriestProcessMemUsageKB:
+
+                hungriestProcessName = name
+                hungriestProcessMemUsageKB = memUsage[name]
+
+        self.runningProcesses = len(memUsage)
+        self.totalMemoryUsageMB = totalMemUsageKB / 1000
+        self.hungriestProcessMemUsageMB = hungriestProcessMemUsageKB / 1000
+        self.hungriestProcessName = hungriestProcessName
 
 if __name__ == "__main__":
 
@@ -61,4 +41,4 @@ if __name__ == "__main__":
 
     print(f"Total running processes: {currentMemoryState.runningProcesses}")
     print(f"Total memory usage: {currentMemoryState.totalMemoryUsageMB} MB")
-    print(f"Hungriest process: {currentMemoryState.hungriestProcessName} ({currentMemoryState.hungriestProcessMemoryUsageMB} MB)")
+    print(f"Hungriest process: {currentMemoryState.hungriestProcessName} ({currentMemoryState.hungriestProcessMemUsageMB} MB)")
